03252890d2693f7abdf238075da8dfafb3c19fe5
Rahul-k14 <46915001+Rahul-k14@users.noreply.github.com>
2019-09-07 13:53:57 +0530

    Update create_instance.tf

--- create_instance.tf
+++ create_instance.tf
@@ -8,15 +8,15 @@ resource "aws_instance" "example" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "example1" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "example2" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
 }
-resource "aws_instance" "example" {
+resource "aws_instance" "example3" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
 }
